78295c68c165eb4a9a12ce1b877d7e05e469fde3
johnwr-response <52628290+johnwr-response@users.noreply.github.com>
2020-06-12 16:38:23 +0200

    Moved package

---
+++ src/main/java/no/responseweb/imagearchive/fileinfoservice/FileInfoServiceApplication.java
@@ -0,0 +1,13 @@
+package no.responseweb.imagearchive.fileinfoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileInfoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileInfoServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/no/responseweb/imagearchive/fileinfoservice/FileInfoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package no.responseweb.imagearchive.fileinfoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileInfoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
