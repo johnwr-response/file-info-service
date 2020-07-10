0ae9771c99330c2a810ed809dfad899a8658f001
johnwr-response <52628290+johnwr-response@users.noreply.github.com>
2020-07-10 13:06:58 +0200

    Made duplication detection work for images fetcher returns 404 when item not found too many many other cleanups and improvements

--- src/main/java/no/responseweb/imagearchive/fileinfoservice/web/controllers/FileEntityController.java
+++ src/main/java/no/responseweb/imagearchive/fileinfoservice/web/controllers/FileEntityController.java
@@ -2,8 +2,7 @@ package no.responseweb.imagearchive.fileinfoservice.web.controllers;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import no.responseweb.imagearchive.filestoredbservice.mappers.FileEntityMapper;
-import no.responseweb.imagearchive.filestoredbservice.repositories.FileEntityRepository;
+import no.responseweb.imagearchive.filestoredbservice.services.FileEntityService;
 import no.responseweb.imagearchive.model.FileEntityDto;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,23 +10,21 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.UUID;
-import java.util.stream.Collectors;
 
 @Slf4j
 @RequiredArgsConstructor
 @RestController
 public class FileEntityController {
 
-    private final FileEntityRepository fileEntityRepository;
-    private final FileEntityMapper fileEntityMapper;
+    private final FileEntityService fileEntityService;
 
     @GetMapping("api/v1/fileEntity/{fileEntityId}")
     public FileEntityDto getFileEntity(@PathVariable UUID fileEntityId) {
         log.info("Finding fileEntity: {}", fileEntityId);
-        return fileEntityMapper.fileEntityToFileEntityDto(fileEntityRepository.findFirstById(fileEntityId));
+        return fileEntityService.findFirstById(fileEntityId);
     }
     @GetMapping("api/v1/fileEntity/{fileEntityId}/listAllExcept")
     public List<FileEntityDto> getAllFileEntitiesExceptCurrent(@PathVariable UUID fileEntityId) {
-        return fileEntityRepository.findDistinctByIdNotLike(fileEntityId).stream().map(fileEntityMapper::fileEntityToFileEntityDto).collect(Collectors.toList());
+        return fileEntityService.findDistinctByIdNotLike(fileEntityId);
     }
 }
